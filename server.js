0c5fde3aed0dbe47d301290d26d8edcfa03ed78f
Rakesh KB <rakesh.kb@medibox.in>
2017-09-22 17:28:58 +0530

    Installed nodemon and run the website with a message for the first time

---
+++ server.js
@@ -0,0 +1,25 @@
+/**
+ * Created by rakesh on 22/09/17.
+ */
+
+var express = require('express'),
+    app = express(),
+    bodyParser = require('body-parser'),
+    mongoose = require('mongoose'),
+    path = require('path');
+
+app.use(bodyParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended : false}));
+app.use(express.static(path.join(__dirname,'client')));
+
+app.get('/',function(req,res){
+    res.sendfile(__dirname+'/client/index.html');
+    // res.render('index',{title : 'Computer Not Working'});
+});
+
+app.listen(9000);
+
+console.log('server started on 9000');
+module.exports = app;
+
